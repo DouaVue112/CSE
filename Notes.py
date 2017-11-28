1ec6b717cca7c8b7a5881ef69f6ee66ee00c64e8
DouaVue112 <31044498+DouaVue112@users.noreply.github.com>
2017-11-28 10:46:18 -0800

    added preious notes

--- Notes.py
+++ Notes.py
@@ -1,1 +1,27 @@
 # This is working
+print("hello world")
+
+# Doua Vue
+# (this is python 2.7)
+
+print(3+5)
+print(5-3)
+print(3*5)
+print(6/2)
+print(3** 2)
+
+print # this makes a new line. In python 3.6, it look like: print()
+print("see if you can figure this out")
+print(13 % 12)
+
+car_name= "Doua Vue"
+car_type= "tesla"
+car_cylinders= 8
+car_mpg = 9000.1
+
+# Inline car
+print("I have a car called the %s" % car_name)
+print("I have a car called the %s. It's a %s." % car_name, car_type)
+# asking for unput
+name = input("what is your name")# In the python
+print ("")
